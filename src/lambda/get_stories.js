56a1317663d1d17da881458cd968b3fbc6ef0c27
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2021-11-27 22:04:28 +0500

    bug fix
    bug fix

--- src/lambda/get_stories.js
+++ src/lambda/get_stories.js
@@ -6,16 +6,6 @@ var requestOptions = {
 
 fetch("https://auth-practice-4b9a7-default-rtdb.firebaseio.com/stories.json?auth=1yovutZkKbzxNzz45YLfnmirBKMy3VIwXeJsjSy0", requestOptions)
   .then(response => response.text())
-  .then(result => 
-            return{
-            statusCode: 200,
-            body: result
-          } 
-  )
-  .catch(error => 
-              return{
-            statusCode: 200,
-            body: error
-          } 
-  );
+  .then(result => return {statusCode: 200,body: result}   )
+  .catch(error =>  return {statusCode: 200, body: error} );
   };
