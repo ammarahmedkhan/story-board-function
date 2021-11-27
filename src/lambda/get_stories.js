c4160418628c5d8c31e4bd7674f6b8527688f5bb
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2021-11-27 22:00:42 +0500

    new way to get data
    new way to get data

--- src/lambda/get_stories.js
+++ src/lambda/get_stories.js
@@ -1,50 +1,21 @@
-import * as admin from 'firebase-admin';
-
-//global.self = {fetch: require('node-fetch')}
-
-var serviceAccount = {
-    //.....
-}
-var logs = [];
-
-   const firebaseConfig = {
-    apiKey: "AIzaSyCpsac9VVt5uE0AF2MVk8kSVMAWFcewFfM",
-    authDomain: "auth-practice-4b9a7.firebaseapp.com",
-    databaseURL: "https://auth-practice-4b9a7-default-rtdb.firebaseio.com",
-    projectId: "auth-practice-4b9a7",
-    storageBucket: "auth-practice-4b9a7.appspot.com",
-    messagingSenderId: "677785265170",
-    appId: "1:677785265170:web:a3a6f44134e47c836fa4cb",
-    measurementId: "G-PYBLC4DJX2"
-  };
-  
-const app = admin.initializeApp(
-firebaseConfig
-);
-if( app === undefined ){
-logs.push('initializeApp is undefined');
-}
-else{logs.push('initializeApp is ok');}
 exports.handler = async (event, context)=>{
-logs.push('reached function call');
-	
-        let ref = await app.database().ref("/stories/");
-	if(ref === undefined){logs.push('ref is undefined');
-          return{
-            statusCode: 200,
-            body: logs
-	}
-	}
-	else{
-	ref.on("value", (snapshot) => {
-	 console.log("got inside the firebase snapshot callback!");
-         const storiesArray = snapshot.val();
-          return{
-            statusCode: 200,
-            body: storiesArray
-          }
-	});
-}
-//if the code above hasnt run
-//      return{statusCode: 200,body: 'didnt get the data!'}
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
 };
+
+fetch("https://auth-practice-4b9a7-default-rtdb.firebaseio.com/stories.json?auth=1yovutZkKbzxNzz45YLfnmirBKMy3VIwXeJsjSy0", requestOptions)
+  .then(response => response.text())
+  .then(result => 
+            return{
+            statusCode: 200,
+            body: result
+          } 
+  )
+  .catch(error => 
+              return{
+            statusCode: 200,
+            body: error
+          } 
+  );
+  };
