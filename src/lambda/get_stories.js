ad344598efd9a582a5ee46c39c3fd59e0cfcee60
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2021-11-27 22:12:40 +0500

    updated api code
    updated api fetch code from 
    https://github.com/DavidWells/netlify-functions-workshop/blob/997d12180e776f6e812af630fc52a3d28c928275/lessons-code-complete/use-cases/5-fetching-data/functions/node-fetch/node-fetch.js

--- src/lambda/get_stories.js
+++ src/lambda/get_stories.js
@@ -1,11 +1,25 @@
-exports.handler = async (event, context)=>{
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+const fetch = require('node-fetch')
 
-fetch("https://auth-practice-4b9a7-default-rtdb.firebaseio.com/stories.json?auth=1yovutZkKbzxNzz45YLfnmirBKMy3VIwXeJsjSy0", requestOptions)
-  .then(response => response.text())
-  .then(result => return {statusCode: 200,body: result}   )
-  .catch(error =>  return {statusCode: 200, body: error} );
-  };
+const API_ENDPOINT = 'https://auth-practice-4b9a7-default-rtdb.firebaseio.com/stories.json?auth=1yovutZkKbzxNzz45YLfnmirBKMy3VIwXeJsjSy0'
+
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
+}
