4229b093f6beb516e560293eb001eba9f2b22366
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2021-11-27 16:09:09 +0500

    firebase stories fetch endpoint necessary settings
    firebase stories fetch endpoint necessary settings

--- webpack.server.js
+++ webpack.server.js
@@ -1,1 +1,40 @@
 require("dotenv").config();
+const path = require('path');
+const pkg = require('./package')
+const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+
+
+const externals = [
+    'firebase-admin',
+    'lodash'
+]
+
+const genPackage = () => ({
+    name: 'functions',
+    private: true,
+    main: 'get_stories.js',
+    author:"@ammar",
+    license: 'MIT',
+    dependencies: externals.reduce(
+    (acc, name) =>
+        Object.assign({}, acc, {
+        [name]:
+            pkg.dependencies[name] ||
+            pkg.devDependencies[name]
+        }),
+    {}
+    )
+})
+
+
+module.exports = {
+    target: 'node',
+    resolve: {
+        mainFields: ['module', 'main']
+    },
+    externals: externals.reduce(
+        (acc, name) => Object.assign({}, acc, { [name]: true }),
+        {}
+    ),
+    plugins: [new GenerateJsonPlugin('package.json', genPackage())]
+}
