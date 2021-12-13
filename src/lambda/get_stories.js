051448beb1f898e33f867bd1cf5aa8d96b7f78cf
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2021-12-13 23:17:37 +0500

    new approach
    new approach

--- src/lambda/get_stories.js
+++ src/lambda/get_stories.js
@@ -1,25 +1,22 @@
 const fetch = require('node-fetch').default;
 
-const checkStatus = (res) => {
-  if (res.ok) { // res.status >= 200 && res.status < 300
-      return res.json()
-  } else {
-      throw new Error(res.statusText);
-  }
-}
 const API_ENDPOINT = 'https://auth-practice-4b9a7-default-rtdb.firebaseio.com/stories.json?auth=1yovutZkKbzxNzz45YLfnmirBKMy3VIwXeJsjSy0'
 
-exports.handler = async (event, context) => {
-  try {
-    const response = await fetch(API_ENDPOINT)
-    const data = await checkStatus(response)
-    callback(null, {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+async function call(api){
+    let res = await fetch(api, { // await helps you to wait here to make your code sync
+        method: "get" 
     })
-  } catch (error) {
-    callback(error)
-  }
+    const json = res.json() 
+    return json ;                                
+}
 
+exports.handler = async (event, context) => {
+
+try {
+const response = await call(API_ENDPOINT); //use try catch to catch error
+console.log(response);
+}
+catch (error) {
+console.log(`Error ${error}`);
+}
 }
